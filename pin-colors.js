18a4c24fd3fd2bf7878e40596e6d1e8eacf7d67a
Olivier Heitz <635761+olivierhey@users.noreply.github.com>
2026-03-31 15:05:29 +0200

    Update pin-colors.js

--- pin-colors.js
+++ pin-colors.js
@@ -22,6 +22,7 @@
     'classic-days-grand-meeting': '#FABD61',
     'bremen-classic-motorshow': '#FABD61',
     'interclassics-maastricht': '#F19E70',
+    'carfest-silverstone': '#B579F3',
   };
   function makePinDataUri(color) {
     const svg = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="10" cy="10" r="8" fill="#2a2a3a" stroke="${color}" stroke-width="3"/></svg>`;
